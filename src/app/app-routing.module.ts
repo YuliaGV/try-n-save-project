531c76b08bdf53b0f83cc21ec1ab96e155daa1b8
YuliaGV <ygaviria7@gmail.com>
2025-07-19 13:28:10 -0500

    basic-structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'expenses', loadChildren: () => import('./features/expenses/expenses.module').then(m => m.ExpensesModule) }, { path: 'expenses', loadChildren: () => import('./features/expenses/expenses.module').then(m => m.ExpensesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'try-n-save-project';
+
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Expense Tracker App'; 
+  isHandset: boolean = false; 
+  private handsetSubscription: Subscription = new Subscription(); 
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+  
+    this.handsetSubscription = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches), 
+        shareReplay() 
+      )
+      .subscribe(matches => {
+        this.isHandset = matches; 
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.handsetSubscription.unsubscribe();
+  }
 }
